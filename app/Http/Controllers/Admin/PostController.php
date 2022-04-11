9339be77106ecced6f48ded17a83a8e15021705e
marco maione <maionemarco14@gmail.com>
2022-04-11 17:16:20 +0200

    aggiornamento

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -6,6 +6,7 @@ use App\Category;
 use App\Http\Controllers\Controller;
 use App\Post;
 use App\Tag;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
@@ -87,6 +88,10 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
+        $now = Carbon::now();
+
+        $postDateTime = Carbon::create($post->created_at);
+
         return view ('admin.post.show', compact('post'));
     }
 
